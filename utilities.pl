37ca62728aabae0573c7ccdd15e8db4280f14cfa
Yasha <yashar@rezilion.com>
2021-11-02 22:12:24 +0200

    Move merge_pieces predicate to utilities.pl

---
+++ utilities.pl
@@ -0,0 +1,10 @@
+merge_pieces([], Pieces, Pieces).
+merge_pieces(Pieces, [], Pieces).
+merge_pieces([Piece1@X1/Y1 | Rest1], [Piece2@X2/Y2 | Rest2], Result) :-
+    (Y1 < Y2;
+    Y1 == Y2, X1 > X2),
+    merge_pieces([Piece1@X1/Y1 | Rest1], Rest2, TempResult),
+    Result = [Piece2@X2/Y2 | TempResult], !
+    ;
+    merge_pieces(Rest1, [Piece2@X2/Y2 | Rest2], TempResult),
+    Result = [Piece1@X1/Y1 | TempResult], !.
