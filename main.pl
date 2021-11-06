1a5ba5f70f0568250c664b2d1596abdd665c8104
Yasha <yashar@rezilion.com>
2021-11-06 15:01:47 +0200

    Cleaning code

--- main.pl
+++ main.pl
@@ -10,14 +10,7 @@
 :- [board].
 
 play :-
-    %Black = [rook@2/8, rook@4/8, king@8/8],
-    %White = [king@1/1, rook@2/2],
-    Black = [rook@1/8, knight@2/8, bishop@3/8, queen@4/8, king@5/8, bishop@6/8, knight@7/8, rook@8/8,
-            pawn@1/7, pawn@2/7, pawn@3/7, pawn@4/7, pawn@5/7, pawn@6/7, pawn@7/7, pawn@8/7],
-    White = [pawn@1/2, pawn@2/2, pawn@3/2, pawn@4/2, pawn@5/2, pawn@6/2, pawn@7/2, pawn@8/2,
-            rook@1/1, knight@2/1, bishop@3/1, queen@4/1, king@5/1, bishop@6/1, knight@7/1, rook@8/1],
-    %White = [rook@1/8, bishop@6/8], %Swap that in when debugging
-    %Black = [rook@1/7, bishop@4/8],
+    pieces_full_board(White vs Black),
     get_user_difficulty(Difficulty), !,
     generate_board(White vs Black, Board),
     print_board(Board),
@@ -159,24 +152,18 @@ get_user_move(Type@Origin goto Position) :-
     (
         Input = (Type@Origin goto Position)
         ;
-        (
-            Input \= 'ff',
-            write('Invalid move, try again'), nl,
-            get_user_move(Type@Origin goto Position)
-        )
+        Input \= 'ff',
+        write('Invalid move, try again'), nl,
+        get_user_move(Type@Origin goto Position)
     ).
 
 get_user_difficulty(Difficulty) :-
-    write('Choose a difficulty level (easy/medium/hard/hell):'), nl,
+    write('Choose a difficulty level (easy / medium / hard):'), nl,
     read(Input),
     (
-        (
-            difficulty(Input, opening, _),
-            Difficulty = Input
-        )
+        difficulty(Input, opening, _),
+        Difficulty = Input
         ;
-        (
-            write('Invalid difficulty, try again.'), nl,
-            get_user_difficulty(Difficulty)
-        )
+        write('Invalid difficulty, try again.'), nl,
+        get_user_difficulty(Difficulty)
     ).
