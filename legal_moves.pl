4671bb1f39e3390d5c2e2112e7d827527a09b5a4
Yasha <yashar@rezilion.com>
2021-11-06 20:20:40 +0200

    Add comments to legal_moves

--- legal_moves.pl
+++ legal_moves.pl
@@ -1,15 +1,18 @@
 :- [operators].
 :- [utilities].
 
+% Generate all valid moves from given position for Color
 valid_moves(PlayerPieces vs OpponentPieces, Color, LastMove, Moves) :-
 	findall(Move, valid_move(PlayerPieces,PlayerPieces vs OpponentPieces, Color, LastMove, Move), Moves).
 
+% Find a valid move from the given position for Color
 valid_move([Type@Origin | Rest], Pieces, Color, LastMove, Move) :-
 	legal_move(Type@Origin, Pieces, Color, LastMove, Position),
 	Move = (Type@Origin goto Position)
 	;
 	valid_move(Rest, Pieces, Color, LastMove, Move).
 
+% Legal moves for each piece
 legal_move(rook@Origin, Pieces, _, _, Position) :-
 	straight(Origin, Pieces, Position).
 legal_move(bishop@Origin, Pieces, _, _, Position) :-
@@ -32,6 +35,7 @@ legal_move(pawn@Origin, PlayerPieces vs OpponentPieces, Color, LastMove, Positio
 	pawn_move(Origin, PlayerPieces vs OpponentPieces, Color, Position);
 	pawn_take(Origin, PlayerPieces vs OpponentPieces, Color, LastMove, Position).
 
+% Check check!
 check(PlayerPieces vs OpponentPieces, Colour, LastMove) :-
 	member(king@Position, PlayerPieces),
 	member(Piece@PiecePosition, OpponentPieces),
@@ -39,18 +43,21 @@ check(PlayerPieces vs OpponentPieces, Colour, LastMove) :-
 	switch_colour(Colour, NewColour),
 	legal_move(Piece@PiecePosition, OpponentPieces vs PlayerPieces, NewColour, LastMove, Position).
 
+% Go diagonally as legally allowed
 diagonal(Origin, Pieces, Position) :-
 	move(Origin, up_right, Pieces, Position);
 	move(Origin, up_left, Pieces, Position);
 	move(Origin, down_right, Pieces, Position);
 	move(Origin, down_left, Pieces, Position).
 
+% Go straight as legally allowed
 straight(Origin, Pieces, Position) :-
 	move(Origin, up, Pieces, Position);
 	move(Origin, down, Pieces, Position);
 	move(Origin, right, Pieces, Position);
 	move(Origin, left, Pieces, Position).
 
+% Define directions
 direction(up, 0/1).
 direction(down, 0/(-1)).
 direction(right, 1/0).
@@ -60,6 +67,7 @@ direction(up_left, 1/(-1)).
 direction(down_right, (-1)/1).
 direction(down_left, (-1)/(-1)).
 
+% Define knight moves
 knight_step_diff(1/2).
 knight_step_diff((-1)/2).
 knight_step_diff(1/(-2)).
@@ -74,11 +82,13 @@ knight_step(X/Y, NewX/NewY) :-
 	NewX is X + DX,
 	NewY is Y + DY.
 
+% Pawn rules
 attack_direction(white, up).
 attack_direction(black, down).
 pawn_rank(white, 2).
 pawn_rank(black, 7).
 
+% Make step with pawn
 pawn_step(X/Y, PlayerPieces vs OpponentPieces, Color, Position) :-
 	attack_direction(Color, Direction),
 	step(X/Y, Direction, Position),
@@ -86,6 +96,7 @@ pawn_step(X/Y, PlayerPieces vs OpponentPieces, Color, Position) :-
 	\+ position_taken(Position, PlayerPieces),
 	\+ position_taken(Position, OpponentPieces).
 
+% Step or attack with pawn
 pawn_move(X/Y, PlayerPieces vs OpponentPieces, Color, Position) :-
 	pawn_step(X/Y, PlayerPieces vs OpponentPieces, Color, Position1),
 	(Position = Position1
@@ -95,6 +106,7 @@ pawn_move(X/Y, PlayerPieces vs OpponentPieces, Color, Position) :-
 	pawn_step(Position1, PlayerPieces vs OpponentPieces, Color, Position2),
 	Position = Position2).
 
+% Potential pawn attack squares
 pawn_attack_square(Origin, Color, Position) :-
 	attack_direction(Color, Direction),
 	step(Origin, Direction, Forward),
@@ -103,27 +115,32 @@ pawn_attack_square(Origin, Color, Position) :-
 	step(Forward, left, Position)),
 	valid_square(Position).
 
+% Whether pawn can take
 pawn_take(Origin, _ vs OpponentPieces, Color, LastMove, Position) :-
 	pawn_attack_square(Origin, Color, Position),
 	(position_taken(Position, OpponentPieces)
 	;
 	pawn_en_passant_virtual_position(LastMove, Position)).
 
+% EN PASSANT!!!!!!!!!!!!!!!!
 pawn_en_passant_virtual_position(pawn@X/Y goto X/Y2, X/VirtualY) :-
 	2 is abs(Y2 - Y),
 	VirtualY is ((Y + Y2) / 2).
 
+% Generate some step
 step(X/Y, Direction, NewX/NewY) :-
 	direction(Direction, DX/DY),
 	NewX is X + DX,
 	NewY is Y + DY.
 
+% Make one move
 move(Origin, Direction, PlayerPieces vs OpponentPieces, Position) :-
 	\+ position_taken(Origin, OpponentPieces),
 	step(Origin, Direction, Position),
 	valid_square(Position),
 	\+ position_taken(Position, PlayerPieces).
 
+% Move and keep moving!
 move(Origin, Direction, PlayerPieces vs OpponentPieces, Position2) :-
 	\+ position_taken(Origin, OpponentPieces),
 	step(Origin, Direction, Position1),
@@ -131,6 +148,7 @@ move(Origin, Direction, PlayerPieces vs OpponentPieces, Position2) :-
 	\+ position_taken(Position1, PlayerPieces),
 	move(Position1, Direction, PlayerPieces vs OpponentPieces, Position2).
 
+% Square within bounds
 valid_square(X/Y) :-
 	integer(X),
 	X >= 1,
